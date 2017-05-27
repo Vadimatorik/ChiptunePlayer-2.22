5ab9ab061613f6265624c8603d7e0dd26b6c76fd
Vadimatorik <deryabkinda@mail.ru>
2017-05-27 20:41:58 +0700

    В процессе запуска 5V.

--- ayplayer_port.cpp
+++ ayplayer_port.cpp
@@ -7,6 +7,17 @@ const constexpr pin_config_t ayplayer_pin_cfg[] = {
 	MACRO_PIN_CFG_ADC(PORT_A, PORT_PIN_0),					// ADC_BAT.
 	MACRO_PIN_CFG_ADC(PORT_A, PORT_PIN_4),					// ADC_RIGHT.
 	MACRO_PIN_CFG_ADC(PORT_B, PORT_PIN_1),					// ADC_LEFT.
+	{
+		.port				= PORT_C,
+		.pin_name			= PORT_PIN_0,
+		.mode				= PIN_OUTPUT_MODE,
+		.output_config		= PIN_OUTPUT_PUSH_PULL_CONFIG,
+		.speed				= PIN_LOW_SPEED,
+		.pull				= PIN_NO_PULL,
+		.af					= PIN_AF_NOT_USE,
+		.locked				= PIN_CONFIG_LOCKED,
+		.state_after_init	= PIN_STATE_AFTER_INIT_SET
+	}
 };
 
 
--- main.cpp
+++ main.cpp
@@ -9,14 +9,34 @@ const constexpr rcc_cfg		ay_rcc_cfg = {
 	.pll_count				= sizeof(array_pll_cfg)/sizeof(array_pll_cfg[0])
 };*/
 
+const constexpr pin_config_t ayplayer_pin_cfg11[] = {
+	MACRO_PIN_CFG_ADC(PORT_A, PORT_PIN_0),					// ADC_BAT.
+	MACRO_PIN_CFG_ADC(PORT_A, PORT_PIN_4),					// ADC_RIGHT.
+	MACRO_PIN_CFG_ADC(PORT_B, PORT_PIN_1),					// ADC_LEFT.
+	{
+		.port				= PORT_C,
+		.pin_name			= PORT_PIN_0,
+		.mode				= PIN_OUTPUT_MODE,
+		.output_config		= PIN_OUTPUT_PUSH_PULL_CONFIG,
+		.speed				= PIN_LOW_SPEED,
+		.pull				= PIN_NO_PULL,
+		.af					= PIN_AF_NOT_USE,
+		.locked				= PIN_CONFIG_LOCKED,
+		.state_after_init	= PIN_STATE_AFTER_INIT_SET
+	}
+};
+
+
+
 
 //const constexpr pin					led(a);
-//const constexpr global_port			g_port(a, 1);
+const constexpr global_port			g_port(ayplayer_pin_cfg11, 4);
 //const constexpr rcc					ay_rcc(&ay_rcc_cfg);
 
 uint32_t l = 0;
 int main (void){
-	//g_port.reinit_all_ports();
+	*((uint32_t*)0x40023850) |= 0xFF;
+	g_port.reinit_all_ports();
 	//led.set();
 
 	while(1) {
