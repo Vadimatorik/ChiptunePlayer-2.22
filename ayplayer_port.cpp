09948a63fdb439ab5496e657c6b20f6c859d1896
Vadimatorik <deryabkinda@mail.ru>
2017-06-18 12:49:09 +0700

    Привязка к комиту. Оформление glosbal port.

--- ayplayer_port.cpp
+++ ayplayer_port.cpp
@@ -1,6 +1,6 @@
 #include "ayplayer_port.h"
 
-const constexpr global_port ayplayer_global_port(ayplayer_global_port_pin_cfg, sizeof(ayplayer_global_port_pin_cfg)/sizeof(pin_config_t));
+const constexpr global_port ayplayer_global_port( ayplayer_global_port_pin_cfg, M_SIZE_ARRAY(ayplayer_global_port_pin_cfg) );
 
 int ayplayer_port_init ( void ) {
     return ( ayplayer_global_port.reinit_all_ports() == E_ANSWER_GP::SUCCESS )?0:-1;
