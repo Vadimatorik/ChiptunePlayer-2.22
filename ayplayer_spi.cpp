248dfa107ab0ca57ca78e53e9e3f3311482e0def
Vadimatorik <deryabkinda@mail.ru>
2017-06-25 13:44:05 +0700

    Намечается тест SPI.

---
+++ ayplayer_spi.cpp
@@ -0,0 +1,5 @@
+#include "ayplayer_spi.h"
+
+int ayplayer_spi_init ( void ) {
+    return 0;
+}
---
+++ ayplayer_spi.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "stm32_f20x_f21x_spi.h"
+
+int ayplayer_spi_init ( void );
+
