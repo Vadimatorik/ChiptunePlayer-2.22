edab00a57ec386f010f8ddbf6005a62add060450
Vadimatorik <deryabkinda@mail.ru>
2017-06-26 01:03:53 +0700

    Тест работы от кварца.

--- ayplayer_clock.h
+++ ayplayer_clock.h
@@ -4,10 +4,13 @@
 #include "ayplayer_port.h"
 
 const constexpr pll_cfg< EC_RCC_PLL_SOURCE::HSE, 25, 240, EC_RCC_PLL_P::DIV_2, 5, EC_RCC_AHB_DIV::DIV_1, EC_RCC_APB1_DIV::DIV_4, EC_RCC_APB2_DIV::DIV_2, 3300 >pll_max;
+const constexpr src_dev_cfg< EC_RCC_AHB_DIV::DIV_1, EC_RCC_APB1_DIV::DIV_1, EC_RCC_APB2_DIV::DIV_1 >pll_hse_max;
 
 const constexpr rcc_cfg ayplayer_rcc_cfg = {
     .main_pll_cfg   = &pll_max,
-    .pll_count      = 1,
+    .pll_cfg_count  = 1,
+    .dev_cfg        = &pll_hse_max,
+    .src_cfg_count  = 1,
     .gb             = &ayplayer_global_port
 };
 
