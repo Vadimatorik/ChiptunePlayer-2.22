add8265213c559dca310e54d60199e68c1803075
Vadimatorik <deryabkinda@mail.ru>
2017-07-03 00:53:33 +0700

    Создание объекта SPI проверено в железе.

--- ayplayer_housekeeping.cpp
+++ ayplayer_housekeeping.cpp
@@ -6,11 +6,11 @@
 void housekeeping_thread ( void* arg ) {
     (void) arg;
     TickType_t xLastWakeTime = xTaskGetTickCount ();
-    uint8_t out = 0;
+    uint8_t out[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     while (1) {
-       // spi1.tx ( out );
+        spi1.tx ( ( void* )out, 10, 10 );
        // spi1.rx( in );
-        out++;
+        out[0]++;
         vTaskDelayUntil( &xLastWakeTime, 500 );
     }
 }
--- ayplayer_spi.h
+++ ayplayer_spi.h
@@ -2,15 +2,15 @@
 
 #include "stm32_f20x_f21x_spi.h"
 
-const spi_master_hardware_os< EC_SPI_NAME                :: SPI1,
-                              EC_SPI_CFG_CLK_POLARITY    :: IDLE_0,
-                              EC_SPI_CFG_CLK_PHASE       :: FIRST,
-                              EC_SPI_CFG_NUMBER_LINE     :: LINE_2,
-                              EC_SPI_CFG_ONE_LINE_MODE   :: USE_2_LINE,
-                              EC_SPI_CFG_DATA_FRAME      :: FRAME_8_BIT,
-                              EC_SPI_CFG_FRAME_FORMAT    :: TI,
-                              EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_2,
-                              EC_SPI_CFG_CS              :: DISABLED > spi1;
+extern spi_master_hardware_os< EC_SPI_NAME                :: SPI1,
+EC_SPI_CFG_CLK_POLARITY    :: IDLE_0,
+EC_SPI_CFG_CLK_PHASE       :: FIRST,
+EC_SPI_CFG_NUMBER_LINE     :: LINE_2,
+EC_SPI_CFG_ONE_LINE_MODE   :: USE_2_LINE,
+EC_SPI_CFG_DATA_FRAME      :: FRAME_8_BIT,
+EC_SPI_CFG_FRAME_FORMAT    :: TI,
+EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_2,
+EC_SPI_CFG_CS              :: DISABLED > spi1;
 
 int ayplayer_spi_init ( void );
 
