87aeea2643e9ec51ad692c12a5164be19eeb1fa6
Vadimatorik <deryabkinda@mail.ru>
2017-07-04 12:36:25 +0700

    Починил SPI.

--- ayplayer_spi.cpp
+++ ayplayer_spi.cpp
@@ -2,13 +2,13 @@
 
 const spi_master_hardware_os< EC_SPI_NAME                :: SPI1,
                               EC_SPI_CFG_CLK_POLARITY    :: IDLE_0,
-                              EC_SPI_CFG_CLK_PHASE       :: SECOND,
+                              EC_SPI_CFG_CLK_PHASE       :: FIRST,
                               EC_SPI_CFG_NUMBER_LINE     :: LINE_2,
                               EC_SPI_CFG_ONE_LINE_MODE   :: USE_2_LINE,
                               EC_SPI_CFG_DATA_FRAME      :: FRAME_8_BIT,
-                              EC_SPI_CFG_FRAME_FORMAT    :: TI,
+                              EC_SPI_CFG_FRAME_FORMAT    :: MOTOROLA,
                               EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_2,
-                              EC_SPI_CFG_CS              :: DISABLED > spi1;
+                              EC_SPI_CFG_CS              :: ENABLED > spi1;
 
 void housekeeping_thread ( void* arg ) {
     (void) arg;
