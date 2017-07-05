b53dbcbf8952d69704e4673a8795020469a9fc0d
Vadimatorik <deryabkinda@mail.ru>
2017-07-05 12:27:26 +0700

    Тестовый hello world на ssd1306.

--- ayplayer_lcd.cpp
+++ ayplayer_lcd.cpp
@@ -6,12 +6,14 @@
 
 uint8_t lcd_buffer[1024] = { 0 };
 
+char test_w[] = "Hello World!";
+
 void ayplayer_lcd_init ( void ) {
     ayplayer_lcd.reinit( spi_master_hardware_os< SPI1_CFG_OBJ_PARAM >::instance() );
     memset(lcd_buffer, 0, 1024);
     ayplayer_lcd.reset();
-
-    ayplayer_lcd.fill_rect_to_buffer(1, 1, 100,30 ,1);
+    ayplayer_lcd.print_string_to_buffer(font_gost_type_b_12_rle, 10, 10, (char*)test_w, 1, 1);
+    //ayplayer_lcd.fill_rect_to_buffer(1, 1, 100,30 ,1);
     ayplayer_lcd.update();
 }
 
--- ayplayer_spi.h
+++ ayplayer_spi.h
@@ -5,12 +5,12 @@
 
 #define SPI1_CFG_OBJ_PARAM        EC_SPI_NAME                :: SPI1,           \
                                   EC_SPI_CFG_CLK_POLARITY    :: IDLE_1,         \
-                                  EC_SPI_CFG_CLK_PHASE       :: SECOND,          \
+                                  EC_SPI_CFG_CLK_PHASE       :: SECOND,         \
                                   EC_SPI_CFG_NUMBER_LINE     :: LINE_2,         \
                                   EC_SPI_CFG_ONE_LINE_MODE   :: USE_2_LINE,     \
                                   EC_SPI_CFG_DATA_FRAME      :: FRAME_8_BIT,    \
                                   EC_SPI_CFG_FRAME_FORMAT    :: MOTOROLA,       \
-                                  EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_256,          \
+                                  EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_2,          \
                                   EC_SPI_CFG_CS              :: ENABLED
 
 int ayplayer_spi_init ( void );
