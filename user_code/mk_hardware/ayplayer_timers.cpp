407834261b5acb7914a0b815e7623b00ddb22bc9
Vadim Deryabkin <deryabkinda@mail.ru>
2017-08-06 13:04:04 +0700

    Переименованы таймеры.

--- user_code/mk_hardware/ayplayer_timers.cpp
+++ user_code/mk_hardware/ayplayer_timers.cpp
@@ -1,10 +1,10 @@
 #include "ayplayer_timers.h"
 
 // Генератор частоты AY.
-tim1_or_8_comp_one_channel< TIM1_OR_TIM8::TIM1, 1, 2, EC_TIM_CH_TOGGLE::CH_2, EC_TIM_CH_MODE::N > ay_player_clk_tim;
+tim1_and_8_comp_one_channel< TIM1_OR_TIM8::TIM1, 1, 2, EC_TIM_CH_TOGGLE::CH_2, EC_TIM_CH_MODE::N > ay_player_clk_tim;
 
 // Генератор прерываний.
-tim6_or_7_interrupt< TIM6_OR_TIM7::TIM6, 400, 200 > ay_player_interrupt_ay;
+tim6_and_7_interrupt< TIM6_OR_TIM7::TIM6, 400, 200 > ay_player_interrupt_ay;
 
 void ayplayer_timers_init ( void ) {
     ay_player_interrupt_ay.reinit();
--- user_code/mk_hardware/ayplayer_timers.h
+++ user_code/mk_hardware/ayplayer_timers.h
@@ -3,7 +3,7 @@
 #include "stm32_f20x_f21x_include_module_lib.h"
 #include "ayplayer_nvic.h"
 
-extern tim1_or_8_comp_one_channel< TIM1_OR_TIM8::TIM1, 1, 2, EC_TIM_CH_TOGGLE::CH_2, EC_TIM_CH_MODE::N > ay_player_clk_tim;
-extern tim6_or_7_interrupt< TIM6_OR_TIM7::TIM6, 400, 200 > ay_player_interrupt_ay;
+extern tim1_and_8_comp_one_channel< TIM1_OR_TIM8::TIM1, 1, 2, EC_TIM_CH_TOGGLE::CH_2, EC_TIM_CH_MODE::N > ay_player_clk_tim;
+extern tim6_and_7_interrupt< TIM6_OR_TIM7::TIM6, 400, 200 > ay_player_interrupt_ay;
 
 void ayplayer_timers_init ( void );
