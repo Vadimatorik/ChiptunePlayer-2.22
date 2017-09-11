78cc2fa457309ae3859ee8363f9f76351e3d4fa0
Vadimatorik <Deryabkinda@mail.ru>
2017-09-11 23:02:30 +0700

    Привязка к новому интерфейсу SPI.

--- user_code/mk_hardware/ayplayer_spi.h
+++ user_code/mk_hardware/ayplayer_spi.h
@@ -3,46 +3,46 @@
 #include "include_module_lib.h"
 #include "ayplayer_nvic.h"
 
-/*
- * Для LCD.
- */
-#define SPI1_CFG_OBJ_PARAM        EC_SPI_NAME                :: SPI1,           \
-                                  EC_SPI_CFG_CLK_POLARITY    :: IDLE_1,         \
-                                  EC_SPI_CFG_CLK_PHASE       :: SECOND,         \
-                                  EC_SPI_CFG_NUMBER_LINE     :: LINE_2,         \
-                                  EC_SPI_CFG_ONE_LINE_MODE   :: USE_2_LINE,     \
-                                  EC_SPI_CFG_FRAME_FORMAT    :: MOTOROLA,       \
-                                  EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_2
+//**********************************************************************
+// Для LCD.
+//**********************************************************************
+#define SPI1_CFG_OBJ_PARAM        SPI::CFG::NAME            :: SPI1,           \
+                                  SPI::CFG::CLK_POLARITY    :: IDLE_1,         \
+                                  SPI::CFG::CLK_PHASE       :: SECOND,         \
+                                  SPI::CFG::NUMBER_LINE     :: LINE_2,         \
+                                  SPI::CFG::ONE_LINE_MODE   :: USE_2_LINE,     \
+                                  SPI::CFG::FRAME_FORMAT    :: MOTOROLA,       \
+                                  SPI::CFG::BAUDRATE_DEV    :: DEV_2
 
-/*
- * Для micro-sd
- */
-#define SPI2_SLOW_CFG_OBJ_PARAM   EC_SPI_NAME                :: SPI2,           \
-                                  EC_SPI_CFG_CLK_POLARITY    :: IDLE_0,         \
-                                  EC_SPI_CFG_CLK_PHASE       :: FIRST,          \
-                                  EC_SPI_CFG_NUMBER_LINE     :: LINE_2,         \
-                                  EC_SPI_CFG_ONE_LINE_MODE   :: USE_2_LINE,     \
-                                  EC_SPI_CFG_FRAME_FORMAT    :: MOTOROLA,       \
-                                  EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_2
+//**********************************************************************
+// Для micro-sd
+//**********************************************************************
+#define SPI2_SLOW_CFG_OBJ_PARAM   SPI::CFG::NAME            :: SPI2,           \
+                                  SPI::CFG::CLK_POLARITY    :: IDLE_0,         \
+                                  SPI::CFG::CLK_PHASE       :: FIRST,          \
+                                  SPI::CFG::NUMBER_LINE     :: LINE_2,         \
+                                  SPI::CFG::ONE_LINE_MODE   :: USE_2_LINE,     \
+                                  SPI::CFG::FRAME_FORMAT    :: MOTOROLA,       \
+                                  SPI::CFG::BAUDRATE_DEV    :: DEV_2
 
-#define SPI2_FAST_CFG_OBJ_PARAM   EC_SPI_NAME                :: SPI2,           \
-                                  EC_SPI_CFG_CLK_POLARITY    :: IDLE_0,         \
-                                  EC_SPI_CFG_CLK_PHASE       :: FIRST,          \
-                                  EC_SPI_CFG_NUMBER_LINE     :: LINE_2,         \
-                                  EC_SPI_CFG_ONE_LINE_MODE   :: USE_2_LINE,     \
-                                  EC_SPI_CFG_FRAME_FORMAT    :: MOTOROLA,       \
-                                  EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_2
+#define SPI2_FAST_CFG_OBJ_PARAM   SPI::CFG::NAME            :: SPI2,           \
+                                  SPI::CFG::CLK_POLARITY    :: IDLE_0,         \
+                                  SPI::CFG::CLK_PHASE       :: FIRST,          \
+                                  SPI::CFG::NUMBER_LINE     :: LINE_2,         \
+                                  SPI::CFG::ONE_LINE_MODE   :: USE_2_LINE,     \
+                                  SPI::CFG::FRAME_FORMAT    :: MOTOROLA,       \
+                                  SPI::CFG::BAUDRATE_DEV    :: DEV_2
 
-/*
- * Для сдвигового регистра и потенциометров.
- */
-#define SPI3_CFG_OBJ_PARAM        EC_SPI_NAME                :: SPI3,           \
-                                  EC_SPI_CFG_CLK_POLARITY    :: IDLE_0,         \
-                                  EC_SPI_CFG_CLK_PHASE       :: FIRST,          \
-                                  EC_SPI_CFG_NUMBER_LINE     :: LINE_2,         \
-                                  EC_SPI_CFG_ONE_LINE_MODE   :: USE_2_LINE,     \
-                                  EC_SPI_CFG_FRAME_FORMAT    :: MOTOROLA,       \
-                                  EC_SPI_CFG_BAUD_RATE_DEV   :: DEV_2
+//**********************************************************************
+// Для сдвигового регистра и потенциометров.
+//**********************************************************************
+#define SPI3_CFG_OBJ_PARAM        SPI::CFG::NAME            :: SPI3,           \
+                                  SPI::CFG::CLK_POLARITY    :: IDLE_0,         \
+                                  SPI::CFG::CLK_PHASE       :: FIRST,          \
+                                  SPI::CFG::NUMBER_LINE     :: LINE_2,         \
+                                  SPI::CFG::ONE_LINE_MODE   :: USE_2_LINE,     \
+                                  SPI::CFG::FRAME_FORMAT    :: MOTOROLA,       \
+                                  SPI::CFG::BAUDRATE_DEV    :: DEV_2
 
 extern spi_master_8bit_hardware_os < SPI1_CFG_OBJ_PARAM > spi1;
 extern spi_master_8bit_hardware_os < SPI2_SLOW_CFG_OBJ_PARAM > spi2_slow;
