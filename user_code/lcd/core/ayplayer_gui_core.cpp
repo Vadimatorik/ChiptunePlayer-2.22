b2a08b666475f506b483940449bbe053305ad152
Vadimatorik <Deryabkinda@mail.ru>
2017-10-14 22:04:28 +0700

    Проверяем карту при включении нормально.

--- user_code/lcd/core/ayplayer_gui_core.cpp
+++ user_code/lcd/core/ayplayer_gui_core.cpp
@@ -75,12 +75,11 @@ void ayplayer_gui_core_task ( __attribute__((unused)) void* param ) {
     // Составить список PSG файлов, если нет такого на карте.
     FIL file_list;
     fr = f_open( &file_list, "psg_list.txt", FA_READ );
-    if ( fr == FR_NO_FILE )     ayplayer_sd_card_scan( path_dir, &m_cont );
+    if ( fr == FR_NO_FILE ) {
+        ayplayer_sd_card_scan( path_dir, &m_cont );
+    }
 
     //uint8_t b_buf_nember;
-    while(1){};
-
-
     /*
 
     // Статус бар. Он есть во всех неигровых окнах.
