092d2d7f51c7c36b757032ab6983bf0b3a359a24
Vadimatorik <Deryabkinda@mail.ru>
2017-10-19 22:33:37 +0700

    Вернул сканирование только при отсутствии файлов.

--- user_code/lcd/core/ayplayer_gui_core.cpp
+++ user_code/lcd/core/ayplayer_gui_core.cpp
@@ -89,11 +89,11 @@ void ayplayer_gui_core_task ( __attribute__((unused)) void* param ) {
 
     // Сюда пришли точно с рабочей картой.
     // Составить список PSG файлов, если нет такого на карте.
-    //FIL file_list;
-  //  fr = f_open( &file_list, "psg_list.txt", FA_READ );
-  //  if ( fr == FR_NO_FILE ) {
+    FIL file_list;
+    fr = f_open( &file_list, "psg_list.txt", FA_READ );
+    if ( fr == FR_NO_FILE ) {
         ayplayer_sd_card_scan( path_dir, &m_cont );
- //   }
+    }
 
     // Формируем главное окно.
     select_window_main();
