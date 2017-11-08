029a9ac8658d6554540598b1f3304843a87533d9
Vadim Deryabkin <deryabkinda@mail.ru>
2017-11-08 12:52:21 +0700

    RCC добавлен.

--- user_code/mk_hardware/ayplayer_clock.cpp
+++ user_code/mk_hardware/ayplayer_clock.cpp
@@ -1,11 +1,39 @@
 #include "ayplayer_clock.h"
 
-rcc ayplayer_rcc( nullptr );
+//const constexpr src_dev_cfg < EC_RCC_AHB_DIV::DIV_2, EC_RCC_APB1_DIV::DIV_1, EC_RCC_APB2_DIV::DIV_1 > pll_hse_max;
 
+const rcc_cfg ay_player_clock_cfg[] = {
+    {
+        . osc_cfg = {
+            .OscillatorType = RCC_OSCILLATORTYPE_HSE,
+            .HSEState       = RCC_HSE_ON,
+            .LSEState       = RCC_LSE_OFF,
+            .HSIState       = RCC_HSI_OFF,
+            .HSICalibrationValue    = RCC_HSICALIBRATION_DEFAULT,
+            .LSIState               = RCC_LSI_OFF,
+            .PLL                    = {                                         // Не используется. Значения на угад в реальном диапазоне.
+                .PLLState               = RCC_PLL_OFF,
+                .PLLSource              = RCC_PLLSOURCE_HSE,
+                .PLLM                   = 0,
+                .PLLN                   = 192,
+                .PLLP                   = RCC_PLLP_DIV2,
+                .PLLQ                   = 2
+            }
+        },
+        .clk_cfg = {
+            .ClockType      = RCC_CLOCKTYPE_SYSCLK,
+            .SYSCLKSource   = RCC_SYSCLKSOURCE_HSE,
+            .AHBCLKDivider  = RCC_SYSCLK_DIV2,
+            .APB1CLKDivider = RCC_HCLK_DIV1,
+            .APB2CLKDivider = RCC_HCLK_DIV1,
+        },
+        . f_latency = FLASH_LATENCY_0
+    }
+};
+
+rcc ayplayer_rcc( ay_player_clock_cfg, 1 );
 
 int ayplayer_clock_init ( void ) {
-    //ayplayer_rcc.pll_sysclk_src_clock_set();
-    //ayplayer_rcc.hse_sysclk_src_clock_set();
-    ayplayer_rcc.set_cfg( 0 );
+    while( ayplayer_rcc.set_cfg( 0 ) != RCC_RESULT::OK );
     return 0;
 }
