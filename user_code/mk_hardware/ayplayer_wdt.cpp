7c4c7863523d818bce16e1b848376509e7e94263
Asubuhi824lia <der_anastasi84@mail.ru>
2018-01-05 23:48:12 +0700

    Пустой проект с WDT.

--- user_code/mk_hardware/ayplayer_wdt.cpp
+++ user_code/mk_hardware/ayplayer_wdt.cpp
@@ -1,14 +1,14 @@
 #include "ayplayer_wdt.h"
 
-wdt_cfg ayplayer_wdt_cfg = {
-    .task_prio          = 1,
-    .run_time_ms        = 200,
-    .startup_time_ms    = 200,
-    .service_time_ms    = 5000
+const wdt_cfg ayplayer_wdt_cfg = {
+	.task_prio			= 1,
+	.run_time_ms		= 200,
+	.startup_time_ms	= 500,
+	.service_time_ms	= 5000
 };
 
 wdt ayplayer_wdt ( &ayplayer_wdt_cfg );
 
 void ayplayer_wdt_init ( void ) {
-    ayplayer_wdt.init();
+	ayplayer_wdt.init();
 }
