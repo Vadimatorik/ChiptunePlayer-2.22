1603e5fc7b225d7cb865fcfb98cc8258ed90d523
Vadimatorik <Deryabkinda@mail.ru>
2018-03-09 20:55:32 +0700

    Для того, чтобы QT не ругался наявно указанные неиспользуемые переменные.

---
+++ user_code/gcc_def.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __GNUC__
+#  define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+#else
+#  define UNUSED(x) UNUSED_ ## x
+#endif
+
+#ifdef __GNUC__
+#  define UNUSED_FUNCTION(x) __attribute__((__unused__)) UNUSED_ ## x
+#else
+#  define UNUSED_FUNCTION(x) UNUSED_ ## x
+#endif
+
