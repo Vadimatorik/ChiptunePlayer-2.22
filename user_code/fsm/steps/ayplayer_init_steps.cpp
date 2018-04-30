4cbdcd9139eefc35dfab15433abad38fe49b4a3b
Vadimatorik <deryabkinda@mail.ru>
2018-04-30 10:29:38 +0700

    Изменения в user_code/fsm/steps/ayplayer_init_steps.cpp 1. Перенес мусор в комментах сюда, чтобы потом разобрать.

--- user_code/fsm/steps/ayplayer_init_steps.cpp
+++ user_code/fsm/steps/ayplayer_init_steps.cpp
@@ -98,6 +98,9 @@ int AyPlayer::fsmStepFuncGuiInit ( HANDLER_FSM_INPUT_DATA ) {
 }
 
 int AyPlayer::fsmStepFuncMicroSdInit ( HANDLER_FSM_INPUT_DATA ) {
+	/*!
+	 * Ждем валидную карту.
+	 */
 	do {
 		obj->waitSdCardInsert();
 
@@ -116,6 +119,96 @@ int AyPlayer::fsmStepFuncMicroSdInit ( HANDLER_FSM_INPUT_DATA ) {
 
 	return 0;
 }
+/*
+int AyPlayer::fsmStepFuncIndexingSupportedFiles ( HANDLER_FSM_INPUT_DATA ) {
+	FRESULT r;
+
+	char path[] = "0:/playlist.sys";
+	FILINFO		fi;
+
+	r = f_stat( path, &fi );
+	switch( ( uint32_t )r ) {
+	case FR_OK:
+		obj->l->sendMessage( RTL_TYPE_M::INIT_OK, "The <<0:/playlist.sys>> file exists!" );
+		return 0;
+
+	case FR_NO_FILE:
+		obj->l->sendMessage( RTL_TYPE_M::INIT_ISSUE, "The <<0:/playlist.sys>> file not exists!" );
+		return 1;
+
+	default:
+
+		while(1);
+		return 2;
+	}
+}*/
+
+int AyPlayer::fsmStepFuncIndexingSupportedFiles ( HANDLER_FSM_INPUT_DATA ) {
+	char*			path	=	( char* )pvPortMalloc( 4096 );
+	strcpy( path, "0:");
+	assertParam( path );
+
+	obj->l->sendMessage( RTL_TYPE_M::INIT_OK, "Indexing files start." );
+	obj->indexingSupportedFiles( path );
+	vPortFree( path );
+
+	/*
+	obj->l->sendMessage( RTL_TYPE_M::RUN_MESSAGE_OK, "Creating file <<0:/playlist.sys>> in sd1." );
+
+
+	r = f_open( &fList, "0:/playlist.sys", FA_WRITE | FA_CREATE_ALWAYS);
+	if ( r != FR_OK ) {
+		if ( obj->l->send_message( RTL_TYPE_M::RUN_MESSAGE_OK, "File <<0:/playlist.sys>> in sd1 was not create." ) ) {
+
+		}
+	}
+
+		if ( obj->l->send_message( RTL_TYPE_M::RUN_MESSAGE_OK, "File <<0:/playlist.sys>> in sd1 created." ) != BASE_RESULT::OK ) return 2;
+
+		/// Поиск по маске PSG.
+		FILINFO		f_scan;
+
+		if ( obj->l->send_message( RTL_TYPE_M::RUN_MESSAGE_OK, "Started scanning <<.psg>> file in sd1." ) != BASE_RESULT::OK ) return 2;
+
+		r = f_findfirst( &obj->sd1_fdir, &f_scan, "0:/", "*.psg");
+		while ( r == FR_OK && f_scan.fname[0] ) {
+			char massageBuf[1024];
+			sprintf( massageBuf, "The file <<%s>> is found.", f_scan.fname );
+			if ( obj->l->send_message( RTL_TYPE_M::RUN_MESSAGE_OK, massageBuf ) != BASE_RESULT::OK ) return 2;
+
+			uint32_t	len_file;
+			EC_AY_FILE_MODE_ANSWER r_ay;
+			r_ay = obj->ay_f->psg_file_get_long( f_scan.fname, len_file );
+
+
+			if ( r_ay == EC_AY_FILE_MODE_ANSWER::OK ) {
+				sprintf( massageBuf, "Length file <<%lu>> ticks.", len_file );
+				if ( obj->l->send_message( RTL_TYPE_M::RUN_MESSAGE_OK, massageBuf ) != BASE_RESULT::OK ) return 2;
+
+				if ( writeDataOfTreck( fList, f_scan.fname, len_file ) != 0 ) {
+					if ( obj->l->send_message( RTL_TYPE_M::RUN_MESSAGE_ERROR, "Write list file error!" ) != BASE_RESULT::OK ) return 2;
+					return 1;
+				} else {
+					if ( obj->l->send_message( RTL_TYPE_M::RUN_MESSAGE_OK, "File added to list." ) != BASE_RESULT::OK ) return 2;
+				}
+			} else {
+				if ( obj->l->send_message( RTL_TYPE_M::RUN_MESSAGE_ERROR, "Length was not received." ) != BASE_RESULT::OK ) return 2;
+			}
+
+			r = f_findnext( &obj->sd1_fdir, &f_scan );
+		}
+
+		r = f_close( &fList );
+		if ( r != FR_OK ) {
+			if ( r == FR_DISK_ERR ) return 1;
+			return 3;
+		}*/
+
+		return 0;
+}
+
+
+
 
 
 
